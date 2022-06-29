5011d926f877afb3255cab9d0d2f13729b80241c
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 14:10:30 -0300

    core():clearcode

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.register(cors, {
 app.register(require('./app.js'))
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, (err) => {
+app.listen(server_port, (err) => {
 
     if (err) {
         app.log.error(err)
