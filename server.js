35d4c7ee091ea24452e086aa24ae6d215ec27842
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 12:15:58 -0300

    core():consolelog

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.register(cors, {
 // app.register(require('./app.js'))
 
 app.listen(process.env.PORT || 5000, '0.0.0.0', (err) => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
     if (err) {
         app.log.error(err)
         process.exit(1)
