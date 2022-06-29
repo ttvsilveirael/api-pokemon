78c02faa5ddbca99f3a34f43de057009f2293572
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 13:51:35 -0300

    core():clearcode

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -2,13 +2,13 @@
 const CardController = require("../controllers/card.controller");
 
 module.exports = async function (fastify, opts) {
-    fastify.get('/', async (request, reply) => '');
+    fastify.get('/', async (request, reply) => 'xd');
 
-    fastify.get('/card', async (req, rep) => await CardController.getCard());
-    fastify.get('/card/:id', async (req, rep) => await CardController.getCard(req.params['id']));
+    // fastify.get('/card', async (req, rep) => await CardController.getCard());
+    // fastify.get('/card/:id', async (req, rep) => await CardController.getCard(req.params['id']));
 
-    fastify.put('/card', async (req, rep) => await CardController.insertCard(req));
+    // fastify.put('/card', async (req, rep) => await CardController.insertCard(req));
 
-    fastify.delete('/card', async (req, rep) => await CardController.deleteCard());
-    fastify.delete('/card/:id', async (req, rep) => await CardController.deleteCard(req.params['id']));
+    // fastify.delete('/card', async (req, rep) => await CardController.deleteCard());
+    // fastify.delete('/card/:id', async (req, rep) => await CardController.deleteCard(req.params['id']));
 }
