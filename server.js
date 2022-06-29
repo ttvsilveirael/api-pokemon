dee08595fb1af902845b92c1b4b06817b8f6d57c
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 16:36:17 -0300

    random test

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const port = process.env.PORT || 5000
 
 // Require the framework
 const Fastify = require('fastify')
+const cors = require("fastify-cors")
 
 // Instantiate Fastify with some config
 const app = Fastify({
     logger: true,
     pluginTimeout: 10000
 });
+app.register(cors, {
+    origin: true
+})
 // Register your application as a normal plugin.
 app.register(require('./app.js'))
 
