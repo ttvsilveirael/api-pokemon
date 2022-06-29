dd9e9c5777d64b524474d1053aef75741cfaaaab
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 01:30:39 -0300

    test proc

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ CardRoutes.routes.forEach(route => fastify.route(route));
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 5000 })
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
