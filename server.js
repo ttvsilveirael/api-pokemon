002a405a7bc5862537000262ff86e5e66b377248
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 12:13:47 -0300

    core():addhost

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.register(cors, {
 // Register your application as a normal plugin.
 // app.register(require('./app.js'))
 
-app.listen(process.env.PORT || 5000, (err) => {
+app.listen(process.env.PORT || 5000, '0.0.0.0', (err) => {
     if (err) {
         app.log.error(err)
         process.exit(1)
