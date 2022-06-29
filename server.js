46d84c4b5ad0b41666d38f348b035a198df0fe2b
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 16:22:39 -0300

    random test

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 'use strict'
+
 const PORT = process.env.PORT || 5000
-// Read the .env file.
-require('dotenv').config()
 
 // Require the framework
 const Fastify = require('fastify')
 
 // Instantiate Fastify with some config
-const app  = Fastify({
+const app = Fastify({
     logger: true,
     pluginTimeout: 10000
 });
@@ -15,4 +14,4 @@ const app  = Fastify({
 app.register(require('./app.js'))
 
 // Start listening.
-app.listen(`${PORT}`)
+app.listen({port: PORT, callback: () => console.log("Runing")});
