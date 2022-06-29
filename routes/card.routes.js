e6d40e8386947209417fc32747942e3fe759e585
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 15:39:51 -0300

    remove package

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -1,15 +1,15 @@
 const CardController = require("../controllers/card.controller");
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
+  app.get('/', async function (request, reply) {
     return 'this is an example'
   });
 
-  fastify.get('/card', async (req, rep) => await CardController.getCard());
-  fastify.get('/card/:id', async (req, rep) => await CardController.getCard(req.params['id']));
+  app.get('/card', async (req, rep) => await CardController.getCard());
+  app.get('/card/:id', async (req, rep) => await CardController.getCard(req.params['id']));
 
-  // fastify.put('/card', async (req, rep) => await CardController.insertCard(req));
+  app.put('/card', async (req, rep) => await CardController.insertCard(req));
 
-  // fastify.delete('/card', async (req, rep) => await CardController.deleteCard());
-  // fastify.delete('/card/:id', async (req, rep) => await CardController.deleteCard(req.params['id']));
+  app.delete('/card', async (req, rep) => await CardController.deleteCard());
+  app.delete('/card/:id', async (req, rep) => await CardController.deleteCard(req.params['id']));
 }
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
+'use strict'
+
 // Read the .env file.
 require('dotenv').config()
 
 // Require the framework
 const Fastify = require('fastify')
+const cors = require("fastify-cors")
 
 // Instantiate Fastify with some config
 const app = Fastify({
-    logger: true
+    logger: true,
+    pluginTimeout: 10000
 })
 
-// Register your application as a normal plugin.
-// app.register(require('./app.js'))
-app.get('/', async function (request, reply) {
-    return { hello: 'world' }
+const CardRoutes = require('./routes/card.routes')
+app.register(cors, {
+    origin: true
 })
-app.listen(process.env.PORT || 5000, '0.0.0.0', (err) => {
+
+CardRoutes.routes.forEach(route => app.route(route));
+
+// Register your application as a normal plugin.
+app.register(require('./app.js'))
+
+// Start listening.
+app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
     if (err) {
         app.log.error(err)
         process.exit(1)
