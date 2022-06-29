0fca2a9da1e2eae6c4c2cea70adb6f95f34c7750
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-29 15:31:33 -0300

    Update ignore

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = Fastify({
 // Register your application as a normal plugin.
 // app.register(require('./app.js'))
 fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-});
+    return { hello: 'world' }
+})
 app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
 
     if (err) {
