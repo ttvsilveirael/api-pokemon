774b6d46b66b0e75689065d33936e119a1bbd88d
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-07-01 00:08:47 -0300

    fix():update string

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -10,8 +10,8 @@ class CardController {
             jsonCard['atk'], jsonCard['def'], jsonCard['weekness'], jsonCard['strength']);
 
         return await psdatabase.insert('cards', "nome,type,image,hp,atk,def,weekness,strength",
-            `'${newCard.nome}','${newCard.type}','${newCard.image}','${newCard.hp}','${newCard.atk}','${newCard.def}', 
-            '${newCard.weekness}','${newCard.strength}'`);
+            `"${newCard.nome}","${newCard.type}","${newCard.image}","${newCard.hp}","${newCard.atk}","${newCard.def}", 
+            "${newCard.weekness}","${newCard.strength}"`);
     }
 
     static async deleteCard(id) {
